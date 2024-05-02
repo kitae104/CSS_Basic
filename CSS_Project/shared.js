801cab1e6c1e8b531d9af272c1219909f9a205de
Kitae's Github <aqua0405@gmail.com>
2024-05-02 11:32:18 +0900

    Update shared.js

--- CSS_Project/shared.js
+++ CSS_Project/shared.js
@@ -1,3 +1,19 @@
-let backdrop = document.querySelector('.backdrop');
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
+const selectPlanButtons = document.querySelectorAll('.plan button'); // plan 클래스 안에 있는 button 태그들을 모두 선택
+const modalNoButton = document.querySelector('.modal button');
 
-backdrop.style.display = 'block';
+// backdrop.style.display = 'block';
+console.dir(selectPlanButtons);
+
+for (let button of selectPlanButtons) {
+    button.addEventListener('click', () => {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
+
+modalNoButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+});
